7294ade943bcfba208ff7219ac9316cadede9032
Devan <deshlock010101@hotmai.com>
2022-02-22 11:28:03 -0600

    added user input for getting balance

--- BankEncapsulationExercise/Program.cs
+++ BankEncapsulationExercise/Program.cs
@@ -8,19 +8,20 @@ namespace BankEncapsulationExercise
         {
             BankAccount account1 = new BankAccount();
 
-            Console.Write("What is the amount you would like to deposit?:");
+            Console.Write("What is the amount you would like to deposit?: $");
             double amountToDeposit = double.Parse(Console.ReadLine());
 
             account1.Deposit(amountToDeposit);
             double userBalance = account1.GetBalance();
 
-            Console.WriteLine($"Your balance is ${userBalance}:");
+            //Console.WriteLine($"Your balance is ${userBalance}:");
 
-            //Console.Write("Would you like to know your balance?: ");
-            //if(Console.ReadLine() == "Yes" || "yes")
-            //{
-            //    Console.WriteLine($"Your balance is {userBalance}:");
-            //}
+            Console.Write("Would you like to know your balance? y / n: ");
+            var user_input = Console.ReadLine();
+            if(user_input == "y")
+            {
+                Console.WriteLine($"Your balance is ${userBalance}:");
+            }
             
         }
     }
