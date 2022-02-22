2e55a248be80649d2a4b71148f5d649a2e97ff8e
Devan <deshlock010101@hotmai.com>
2022-02-22 11:18:25 -0600

    completed the exercise

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+        public double Deposit(double amount)
+        {
+            return _balance += amount;
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulationExercise/Program.cs
+++ BankEncapsulationExercise/Program.cs
@@ -6,7 +6,22 @@ namespace BankEncapsulationExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account1 = new BankAccount();
+
+            Console.Write("What is the amount you would like to deposit?:");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            account1.Deposit(amountToDeposit);
+            double userBalance = account1.GetBalance();
+
+            Console.WriteLine($"Your balance is ${userBalance}:");
+
+            //Console.Write("Would you like to know your balance?: ");
+            //if(Console.ReadLine() == "Yes" || "yes")
+            //{
+            //    Console.WriteLine($"Your balance is {userBalance}:");
+            //}
+            
         }
     }
 }
