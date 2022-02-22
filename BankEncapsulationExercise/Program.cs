a1a4a73e157521d9181e263c9af42e2fdbc3a459
Devan <deshlock010101@hotmai.com>
2022-02-22 11:33:03 -0600

    refactored.

--- BankEncapsulationExercise/Program.cs
+++ BankEncapsulationExercise/Program.cs
@@ -12,15 +12,12 @@ namespace BankEncapsulationExercise
             double amountToDeposit = double.Parse(Console.ReadLine());
 
             account1.Deposit(amountToDeposit);
-            double userBalance = account1.GetBalance();
-
-            //Console.WriteLine($"Your balance is ${userBalance}:");
-
+     
             Console.Write("Would you like to know your balance? y / n: ");
             var user_input = Console.ReadLine();
             if(user_input == "y")
             {
-                Console.WriteLine($"Your balance is ${userBalance}:");
+                Console.WriteLine($"Your balance is ${account1.GetBalance()}:");
             }
             
         }
